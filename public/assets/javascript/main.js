bca5c72168130540c9ce963d4f8959f85e1df5b8
StarfoxMulder <cjprestia@gmail.com>
2019-10-08 12:51:50 -0400

    fixed footer - transitioning to mobile layout overhaul

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -18,4 +18,65 @@ $(document).ready(function () {
         console.log("width 0");
     });
 
+    function addFadeIn(parentID) {
+        var num = document.querySelectorAll('#' + parentID + ' .fadeInText').length;
+        var children = document.querySelectorAll('#' + parentID + ' .fadeInText');
+
+        $('#' + parentID + ' .fadeInText').each(function (i) {
+            t = i + 0.5;
+            $(this).css({
+                "animation-delay": t + "s"
+            })
+        });
+    }
+    addFadeIn("tagline");
+
 });
+
+//Add class FadeIn to each child element of parent element w/ special identifier
+// delayed by 0.X seconds per each iteration of loop
+/*  1) Pass in ID of parent div
+    2) count number of relevant children by class .fadeTextIn = F
+    3) Run loop until i == F, starting i at 1
+    4) loop goes through child.fadeTextIn(i) adding class fadeIn; setTimeout(500ms)
+    5) ***Execute page specific functions on load, javascript
+    
+function addFadeIn (parentID) {
+    var num = document.querySelectorAll('#'+parentID+' .fadeTextIn').length;
+    var children = document.querySelectorAll('#'+parentID+' .fadeTextIn');
+
+    for (i=1; i=num; i++){
+        setTimeout(fadeIn(children, i), 500);
+    }
+}
+function fadeIn(children, i){
+    i2 = i-1;
+    children[i2].classList.add("fadeIn");
+}
+*/
+
+//    function addFadeIn(parentID) {
+//     var num = document.querySelectorAll('#' + parentID + ' .fadeInText').length;
+//     var children = document.querySelectorAll('#' + parentID + ' .fadeInText');
+//     var t = [];
+//     console.log("num == " + num);
+//     console.log("children == " + children);
+
+//     for (i = 1; i <= num; i++) {
+//         console.log("loop run #" + i);
+
+//         t[i] = setTimeout(fadeIn(children, i), 4200);
+//         console.log(t);
+//     }
+// }
+
+// function fadeIn(children, i) {
+//     console.log(children);
+//     console.log("fadeIn i == " + i)
+//     i2 = i - 1;
+//     console.log("i2 == " + i2);
+//     console.log(children[i2]);
+//     children[i2].classList.add("fadeIn");
+// }
+
+// addFadeIn("tagline");
